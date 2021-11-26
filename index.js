617e4fd7e2b4cbc1297f208420179f5db6700d42
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-11-26 14:11:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function newQuote() {
-	fetch("quotes.json").then(response => response.json).then(quotes => {
+	fetch("quotes.json").then(response => response.json()).then(quotes => {
 		let number = ~~(Math.random() * quotes.length);
 		document.querySelector(".quote").innerText = quotes[number][0];
 		document.querySelector(".person").innerText = quotes[number][1];
 	});
-	fetch("images/images.json").then(response => response.json).then(images => {
+	fetch("images/images.json").then(response => response.json()).then(images => {
 		document.querySelector("main").setAttribute("style", "background-image: url(images/" + String(~~(Math.random() * images.length)) + ")");
 	})
 }
