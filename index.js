326759adeed09035e1100d5bfce136fc580645f5
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-11-26 14:08:30 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+function newQuote() {
+	fetch("quotes.json").then(response => response.json).then(quotes => {
+		let number = ~~(Math.random() * quotes.length);
+		document.querySelector(".quote").innerText = quotes[number][0];
+		document.querySelector(".person").innerText = quotes[number][1];
+	});
+	fetch("images/images.json").then(response => response.json).then(images => {
+		document.querySelector("main").setAttribute("style", "background-image: url(images/" + String(~~(Math.random() * images.length)) + ")");
+	})
+}
+addEventListener("load", function() {
+	newQuote();
+});
